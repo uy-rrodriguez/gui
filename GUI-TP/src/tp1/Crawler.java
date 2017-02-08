55c458fc7449bf8ea80d6d52db5af4d6e22435e9
Ricardo Rodriguez <rr.ricci@gmail.com>
2017-02-08 18:10:09 +0100

    Main Crawler

---
+++ GUI-TP/src/tp1/Crawler.java
@@ -0,0 +1,23 @@
+package tp1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Crawler extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("layout/application.fxml"));
+		Scene scene = new Scene(root);
+		stage.setTitle("Crawler");
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(Crawler.class, args);
+	}
+}
